de037df2e3c4b4aadccac0d6c1a85db24a6aef3b
Asad <asadaliazam007@gmail.com>
2018-10-28 11:25:43 -0700

    added functionality for todolist

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
-  class TodoList extends Component {
-  render() {
-    return (
-      <div>
-        Todo List
-      </div>
-    )
-  }
+class TodoList extends Component {
+
+
+    render() {
+        const fakeTodos = [
+            { id: 1, text: "buy a car", completed: false, todoClick: () => console.log("clicked!") },
+            { id: 2, text: "play the guitar", completed: true, todoClick: () => console.log("clicked!") },
+            { id: 3, text: "play with cat", completed: false, todoClick: () => console.log("clicked!") }
+        ];
+        return (
+            <div>
+                <ul>
+                    {fakeTodos.map(todo =>
+                        <li
+                            key={todo.id}
+                            onClick={everything.allClick}
+                            style={{
+                                textDecoration: todo.completed ? 'line-through' : 'none'
+                            }}
+                        >
+                            {todo.text}
+                        </li>
+                    )}
+                </ul>      </div>
+        )
+    }
 }
 
 export default TodoList;
